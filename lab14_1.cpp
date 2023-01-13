1a4eabaf96a6dc7455f7a7b0b8d779adafd4527e
Your Name <you@example.com>
2023-01-13 16:12:56 +0700

    lab14_1 finished

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,39 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    
+    for(int i = 0;i<N ;i++){
+        B[0]+=A[i];
+    }
+    B[0] = B[0]/N;
+    double sum = 0;
+    for(int j = 0;j<N ; j++){
+        sum += pow(A[j]-B[0],2);
+    }
+    B[1] = sqrt(sum/(N));
+
+    double Geometric =1;
+    for(int i=0;i<N;i++){
+        Geometric *= A[i];
+    }
+    B[2] = pow(Geometric,pow(N,-1));
+
+    double H = 0;
+    for(int i=0;i<N;i++){
+        H += 1/(A[i]); 
+    }
+    B[3] = N/H;
+
+    B[4]=A[1];
+    B[5]=A[1];
+    for(int i=0;i<N;i++){
+        if(A[i]>B[4]){
+            B[4]=A[i];
+        }
+        if(A[i]<B[5]){
+            B[5]=A[i];
+        }    
+    }
+}
